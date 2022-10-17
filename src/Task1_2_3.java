9d9bcf018f4b3fa50e2a6641f50adc206bbb1e0e
roman <roman-1901@mail.ru>
2022-10-17 21:48:02 +0300

    Homework#2

--- src/Task1_2_3.java
+++ src/Task1_2_3.java
@@ -1,5 +1,25 @@
 public class Task1_2_3 {
     public static void main(String[] args) {
-        System.out.println("Test");
+        var dog = 6;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        dog = dog - 3;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
     }
 }
